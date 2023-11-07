0d24a703cc0663ec7acf9f6a960ba41801946415
troferoller <d.dulger@mail.ru>
2023-11-07 23:39:36 +0300

    Added DragAndDropTest part hw

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Configuration;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.interactions.Actions;
+
+import static com.codeborne.selenide.Condition.text;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    private final SelenideElement dragItem = $("#column-a");
+    private final SelenideElement dropTarget = $("#column-b");
+    @BeforeAll
+    static void configs(){
+        Configuration.browserSize = "2560x1440";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void checkContent() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        Actions actions = new Actions(WebDriverRunner.getWebDriver());
+        actions.dragAndDrop(dragItem.toWebElement(), dropTarget.toWebElement()).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo(element("#column-b"));
+    }
+}
